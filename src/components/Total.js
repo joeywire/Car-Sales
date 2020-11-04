e50c01026f73b30daabc4e96c6c287b7a076255a
joeywire <61367542+joeywire@users.noreply.github.com>
2020-11-03 21:36:34 -0600

    Built total price calc function

--- src/components/Total.js
+++ src/components/Total.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
 const Total = props => {
+
+  const {car} = props;
+
+  const calcTotalPrice = (car) => {
+    let addPrice = 0;
+    if (car.features.length > 0){
+      car.feature.forEach((ft) => addPrice += ft.price)
+    };
+    return car.price + addPrice;
+  }
+
+
   return (
     <div className="content">
-      <h4>Total Amount: ${props.car.price + props.additionalPrice}</h4>
+      <h4>Total Amount: ${calcTotalPrice(car)}</h4>
     </div>
   );
 };
